a04e11e11981af51f6dd1ad7cf5725a5e8bca72c
deshdeepak13 <ddeepakverma0913@gmail.com>
2025-10-12 04:54:05 +0530

    updated llm model for chatbot

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -13,7 +13,7 @@ if (!messages || !Array.isArray(messages)) {
     const response = await axios.post(
   "https://api.groq.com/openai/v1/chat/completions",
   {
-    model: "llama3-70b-8192",
+    model: "llama-3.1-8b-instant",
     messages: [
       {
         role: "system",
